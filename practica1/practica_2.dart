93806aa7fa9c965a62dd41616fbc6ce6113b5cec
Ilesandres <ilessambonibreinerandres@gmail.com>
2024-04-29 11:41:00 -0500

    mapeos

--- practica1/practica_2.dart
+++ practica1/practica_2.dart
@@ -1,6 +1,45 @@
 void main(){
-String Nombre='Andres';
+  
+  final Map<String, dynamic> pokemon={
+    'name':'Ditto',
+    'hp': 100,
+    'isAlive':true,
+    'abilities':<String>['impostor'],
+    'sprites':{
+      1: 'ditto/front.png',
+      2: 'ditto/back.png'
+    }
+    
+  };
+  
+  print(pokemon);
+  
+  print("Name : ${pokemon['name']}");
+  print("Sprites : ${pokemon['sprites']}");
+  print(" ");
+  print("back : ${pokemon['sprites'][2] }");
+  print("front : ${pokemon['sprites'][1] }");
 
-print("Hola $Nombre");
-
+  
+  final Map<int, dynamic> pokemons={
+    1:'ABC',
+    2:'XYZ',
+    3:'123',
+    150:'clean'
+  };
+  
+  
+  
+  
+  print(pokemons);
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
 }
